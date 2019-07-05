41ea0d66cc9ea9aaef9bd06c56e7eb89dd988a3b
Jack Laxson <jackjrabbit@gmail.com>
2019-07-05 02:08:05 -0400

    windows path support, native macos

--- main.go
+++ main.go
@@ -6,19 +6,30 @@ import (
 	"github.com/spf13/viper"
 	"github.com/whatupdave/mcping"
 	"log"
+	"os"
+	"runtime"
 )
 
 const version = "0.0.1"
 
 func main() {
 	fmt.Printf("mcping-bin version %s\n", version)
+	// ref https://github.com/pallets/click/blob/4da5e93cede17262424671208799bc6921dcfa36/click/utils.py#L368-L417
 	viper.AddConfigPath("$HOME/.config/")
+	viper.AddConfigPath("$HOME/Library/Application Support/")
+	cwd, _ := os.Getwd()
+	viper.AddConfigPath(cwd)
+	if runtime.GOOS == "windows" {
+		winAppData := os.Getenv("LOCALAPPDATA")
+		viper.AddConfigPath(winAppData)
+	}
 	viper.SetConfigName("mcping")
+
 	viper.SetDefault("telegraf_server", "localhost:8094")
 	viper.SetDefault("miceraft_server", "localhost:25565")
 	viper.ReadInConfig()
-	var grafServer string
-	grafServer = viper.GetString("telegraf_server")
+
+	grafServer := viper.GetString("telegraf_server")
 	log.Printf("mcping config file: %s", viper.ConfigFileUsed())
 	client, tel_err := telegraf.NewTCP(grafServer)
 	if tel_err != nil {
@@ -28,7 +39,7 @@ func main() {
 	mcServer := viper.GetString("minecraft_server")
 	resp, mcErr := mcping.Ping(mcServer)
 	if mcErr != nil {
-		log.Fatalf("minecraft fail: %s", err)
+		log.Fatalf("minecraft fail: %s", mcErr)
 	}
 	m := telegraf.MeasureInt("mcping-go", "online", resp.Online)
 	write_err := client.Write(m)
